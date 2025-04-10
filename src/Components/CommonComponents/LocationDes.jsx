d8fd3b18cd1e8629468c81cc1e919c4e6bcbd5b7
Hemel <rh189827@gmail.com>
2025-04-10 12:33:01 +0600

    Description area added

---
+++ src/Components/CommonComponents/LocationDes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function LocationDes() {
+  return (
+    <div className="space-y-0.5 sm:space-y-3">
+      <h1 className="text-accent font-bold text-base sm:text-lg">Description</h1>
+      <p className="text-[13px] sm:text-base text-secondary leading-tight sm:leading-relaxed text-justify">
+        Lorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werhoLorem ipsum dolar sit general sac mascho werho
+      </p>
+    </div>
+  )
+}
+
+export default LocationDes
--- src/Components/Pages/LocationsPage.jsx
+++ src/Components/Pages/LocationsPage.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import LocationTexts from '../CommonComponents/LocationTexts'
+import LocationDes from '../CommonComponents/LocationDes'
 
 function Locations() {
   return (
     <div className='mx-4 mt-8'>
       <div className='grid grid-cols-2 gap-5'>
-        <div>
+        <div className='space-y-3'>
           <LocationTexts />
+          <div className='w-full h-0.5 bg-[#E4E4E7]'>
         </div>
+        <LocationDes />
+        <div className='w-full h-0.5 bg-[#E4E4E7]'>
+        </div>
+        </div>
+        
+          
         <div className='space-y-2 sm:space-y-5'>
          <p className='text-sm text-secondary'>Incident Map</p>
          <img src="/FakeMap2.svg" alt="" className='w-full object-cover'/>
-         <p className='text-sm text-secondary'>Start 19.1232, -118.233     End 19.3245, -119.2323</p>
+         <p className='text-[13px] sm:text-base text-secondary leading-tight sm:leading-relaxed text-justify flex justify-around'><span>Start 19.1232, -118.233</span> <span>End 19.3245, -119.2323</span></p>
         </div>
 
       </div>
